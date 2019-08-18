6f7f1f401a3b59526d52aa54229f12d3bcfc1513
Manmath <manmath738@gmail.com>
2019-08-18 21:48:50 +0530

    Changed method Call

--- Hibernate/src/main/java/com/Hibernate/Main/Main.java
+++ Hibernate/src/main/java/com/Hibernate/Main/Main.java
@@ -15,6 +15,8 @@ import org.hibernate.cfg.Configuration;
 import com.Hibernate.Annotation.FruitPrimaryKey;
 import com.Hibernate.Annotation.Fruits;
 import com.Hibernate.OneToOneMapping.StudentDetail;
+import com.Hibernate.OneToOneMappingUniBidirectional.Order;
+import com.Hibernate.OneToOneMappingUniBidirectional.OrderDetail;
 import com.Hibernate.TableCreate.Employee;
 import com.Hibernate.TableCreate.Student;
 import com.Hiberntate.HibernateUtil.HibernateUtil;
@@ -23,49 +25,58 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Student Details
+		// Student Details
 		/*
 		 * Student student=new Student(); student.setId(2);
 		 * student.setName("Manmath M");
-		 */		
-		//Employee details
+		 */
+		// Employee details
 		/*
 		 * Employee employee=new Employee(); employee.setEid(11);
 		 * employee.setEname("Manmath M");
 		 */
-		//Fruit Details
+		// Fruit Details
 		/*
 		 * Fruits fruits=new Fruits(); fruits.setFruitCount(10);
 		 * fruits.setFruitName("Apple"); fruits.setFruitDelivery("New Dehli");
 		 * fruits.setDeliveryDate(new Date());
 		 */
-		//FruitPrimaryKey Details
+		// FruitPrimaryKey Details
 		/*
 		 * FruitPrimaryKey fruitPrimaryKey=new FruitPrimaryKey();
 		 * fruitPrimaryKey.setFruitName("CrusterApple"); FruitPrimaryKey
 		 * fruitPrimaryKey1=new FruitPrimaryKey();
 		 * fruitPrimaryKey1.setFruitName("PineApple");
 		 */
-		//Student and StudentDetail class
-		com.Hibernate.OneToOneMapping.Student studentt=new com.Hibernate.OneToOneMapping.Student();
-		studentt.setStudent_name("Manmath M");
+		// Student and StudentDetail class
+		/*
+		 * com.Hibernate.OneToOneMapping.Student studentt=new
+		 * com.Hibernate.OneToOneMapping.Student();
+		 * studentt.setStudent_name("Manmath M");
+		 * 
+		 * StudentDetail studentDetail=new StudentDetail();
+		 * studentDetail.setStudent_mobile_nuumber("92XXXX92XX"); //This is an important
+		 * step studentDetail.setStudent(studentt);
+		 */
+		// Order and OrderDetail class
+		Order order = new Order();
+		order.setOrderName("Pizza");
 		
-		StudentDetail studentDetail=new StudentDetail();
-		studentDetail.setStudent_mobile_nuumber("92XXXX92XX");
-		//This is an important step
-		studentDetail.setStudent(studentt);
-		
-		HibernateUtil hibernateUtil=new HibernateUtil();
-		hibernateUtil.Hibernate(studentDetail);
+		OrderDetail orderDetail = new OrderDetail();
+		orderDetail.setOrderStatus("Cooking");
+		orderDetail.setOrder(order);
+
+		HibernateUtil hibernateUtil = new HibernateUtil();
+		hibernateUtil.Hibernate(orderDetail);
+		// hibernateUtil.Hibernate(studentDetail);
 		/*
 		 * hibernateUtil.Hibernate(fruitPrimaryKey1);
 		 * hibernateUtil.Hibernate(fruitPrimaryKey);
 		 */
-		//hibernateUtil.Hibernate(fruits);
-		//hibernateUtil.Hibernate(employee);
-		//hibernateUtil.Hibernate(student);
-	
-	
+		// hibernateUtil.Hibernate(fruits);
+		// hibernateUtil.Hibernate(employee);
+		// hibernateUtil.Hibernate(student);
+
 	}
 
 }
