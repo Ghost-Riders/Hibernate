4c810aeed6b78eebad36618b49eb39034fa51cc5
Manmath <manmath738@gmail.com>
2019-08-18 14:08:19 +0530

    Removed Constructor

--- Hibernate/src/main/java/com/Hibernate/TableCreate/Employee.java
+++ Hibernate/src/main/java/com/Hibernate/TableCreate/Employee.java
@@ -10,12 +10,6 @@ public class Employee {
 	private int eid;
 	private String ename;
 
-	public Employee(int eid, String ename) {
-		super();
-		this.eid = eid;
-		this.ename = ename;
-	}
-
 	public int getEid() {
 		return eid;
 	}
